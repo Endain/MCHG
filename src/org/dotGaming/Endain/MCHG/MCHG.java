dc6fd9eecc3ce275645728ac99ec6d079db35925
Endain <endain.gaming@gmail.com>
2012-12-28 17:05:04 -0700

    Add plugin.yml and add base plugin class.

---
+++ src/org/dotGaming/Endain/MCHG/MCHG.java
@@ -0,0 +1,15 @@
+package org.dotGaming.Endain.MCHG;
+ 
+import org.bukkit.plugin.java.JavaPlugin;
+ 
+public final class MCHG extends JavaPlugin {
+	@Override
+    public void onEnable(){
+        // TODO Insert logic to be performed when the plugin is enabled
+    }
+ 
+    @Override
+    public void onDisable() {
+        // TODO Insert logic to be performed when the plugin is disabled
+    }
+}
